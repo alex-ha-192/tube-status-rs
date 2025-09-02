9f11a4cf578a7debb924e0d61df5bf31465a9a39
Alex Hegedus-Adkin <alex@hegedusadkin.co.uk>
2025-09-02 15:58:01 +0000

    Added async and concurrent behaviour

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 use colored::Colorize;
 use serde_json::Value;
 use std::collections::HashMap;
-use std::env;
+use std::thread;
 
 async fn make_request_to_text(url: &str) -> String {
     return reqwest::get(url).await.unwrap().text().await.unwrap();
@@ -16,22 +16,9 @@ fn trim_outer_quotes(s: &mut String) -> String {
     s.clone()
 }
 
-/// This function will trim everything up until the first full stop/period encountered.
-/// This means that this function will truncate a long string to only its first sentence.
-/// This should not be used, --only-one-sentence should be deprecated
-fn trim_full_stop(s: &mut String) -> String {
-    let mut s_iter = s.split(".");
-    let s_upper_section = s_iter
-        .next()
-        .expect("Failed to trim whitespace")
-        .to_string();
-    s_upper_section.clone()
-}
-
 /// This function expects a response v directly from the TfL API and expects it to meet the TfL API's format.
 /// This function cannot be reused for other responses.
-fn print_color_and_desc(v: &Value, args: &Vec<String>) {
-    let exclude_full_stop = args.contains(&"--only-one-sentence".to_string());
+fn print_color_and_desc(v: &Value) {
     let lines_to_colors: HashMap<String, [u8; 3]> = HashMap::from([
         ("Bakerloo".to_string(), [178, 99, 0]),
         ("Central".to_string(), [220, 36, 31]),
@@ -82,35 +69,28 @@ fn print_color_and_desc(v: &Value, args: &Vec<String>) {
         let status_reason_trimmed = status_reason_iter
             .next()
             .expect("Failed to parse status reason");
-        if !exclude_full_stop {
-            println!(
-                "{}: {}",
-                name_of_line.truecolor(rgb[0], rgb[1], rgb[2]),
-                status_reason_trimmed.to_string()
-            );
-        } else {
-            println!(
-                "{}:{}",
-                name_of_line.truecolor(rgb[0], rgb[1], rgb[2]),
-                trim_full_stop(&mut status_reason_trimmed.to_string())
-            );
-        }
+        println!(
+            "{}: {}",
+            name_of_line.truecolor(rgb[0], rgb[1], rgb[2]),
+            status_reason_trimmed.to_string()
+        );
     }
 }
 
-
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = env::args().collect();
-
     println!("/// TFL status ///");
 
-        let tfl_response = make_request_to_text("https://api.tfl.gov.uk/line/mode/tube/status");
+    let tfl_response = make_request_to_text("https://api.tfl.gov.uk/line/mode/tube/status");
 
-        let response: Vec<Value> =
-            serde_json::from_str(tfl_response.await.as_str()).expect("Failed to serialize API response");
+    let response: Vec<Value> = serde_json::from_str(tfl_response.await.as_str())
+        .expect("Failed to serialize API response");
 
-        for entry in response {
-            print_color_and_desc(&entry, &args);
-}
+    // Output order of lines is non-deterministic
+    for entry in response {
+        // print_color_and_desc(&entry);
+        thread::spawn(move || {
+            print_color_and_desc(&entry);
+        });
+    }
 }
